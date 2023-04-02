0758b9a3ef206b409585071cd2ddb7f819256071
Ugyen Lama <ugyenchheda@gmail.com>
2023-04-02 03:09:44 +0300

    Using memory mapping for changing directory

--- ChangeDirectory/Program.cs
+++ ChangeDirectory/Program.cs
@@ -4,9 +4,6 @@ using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ChangeDirectory
 {
     internal class Program
