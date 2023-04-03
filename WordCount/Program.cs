efa329fecf5dcd08ea23e0bac88fd0c012d8537c
Ugyen Lama <ugyenchheda@gmail.com>
2023-04-03 10:27:55 +0300

    final working wordcount solution

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -13,7 +13,6 @@ namespace WordCount
 
             string fileName = args[0];
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
-            int wordCount = 0;
 
             string fileContents = File.ReadAllText(filePath);
 
@@ -21,34 +20,21 @@ namespace WordCount
 
             int newwordCount = words.Length;
 
-            Console.WriteLine("The new file contains {0} words.", newwordCount);
+            Console.WriteLine("My solution: Word Count:  {0}.", newwordCount);
             try
             {
                 using (StreamReader sr = new StreamReader(filePath))
                 {
-                    string line;
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        wordCount += line.Split(' ', '\n', '\r', '\t').Length;
-                    }
+                    char[] splitterChars = { };
+                    int counter = fileContents.Split(splitterChars, StringSplitOptions.RemoveEmptyEntries).Length;
+                    Console.WriteLine("Teacher's Solution: Word Count: {0}.", counter);
                 }
-
-
-                //string contents = File.ReadAllText(filePath);
-                ////string[] words = Regex.Split(contents, @"\s+");
-                //string[] words = contents.Split(new char[] {',',  '.',  ';',  ':', ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-                ////string[] words = Regex.Split(contents, @"\W+");
-
-                var count = File.ReadAllText(filePath).Split(' ').Count();
-                //wordCount = words.Length;
-                Console.WriteLine("Word count is {0}", count);
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error reading the file: " + ex.Message);
             }
 
-            Console.WriteLine("Number of words in the file: " + wordCount);
 
 
 
